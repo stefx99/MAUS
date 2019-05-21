8be454e41391fc6ac4163e204cfe875a59ec52a2
Marko <vukoticmarko99@gmail.com>
2019-05-21 11:21:05 +0200

    Merge branch 'master' of C:\Users\Maki\Desktop\Fakultet\Prva Godina\#2 Drugi Semestar\Uvod u Softversko Inzernjerstvo\MAUS with conflicts.

--- src/packages/view.py
+++ src/packages/view.py
@@ -4,7 +4,7 @@ from PySide2 import QtWidgets, QtCore, QtGui
 from src.model.Node import Node
 from src.packages.HierarchyTreeModel import HierarchyTreeModel
 from src.packages.HierarchyTreeView import HierarchyTreeView
-
+from src.packages.MenuBar import MenuBar
 class MainWindow(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
@@ -20,70 +20,25 @@ class MainWindow(QtWidgets.QWidget):
 
 
         # Setting up and add to layout
-        self.layout = QtWidgets.QHBoxLayout()
+        self.layout = QtWidgets.QGridLayout()
+        self.layout.addWidget(self.tree, 0, 0)
+        self.layout.addWidget(self.pageView, 0, 1)
 
-        self.layout.addWidget(self.tree)
-        self.layout.addWidget(self.pageView)
-
-        # Menu Bar//////
-
-        self.mainMenu = QtWidgets.QMenuBar()
-
-        self.filemenu = self.mainMenu.addMenu("File")
-        self.helpmenu = self.mainMenu.addMenu("Help")
-
-        self.filemenu.addAction("New")
-        self.filemenu.addAction("Open")
-        self.filemenu.addAction("Save")
-        self.filemenu.addAction("Exit")
-        # fileMenu.addMenu("File")
-        # viewMenu.addMenu("View")
-        # editMenu.addMenu("Edit")
-        # helpMenu.addMenu("Help")
-
-        #self.menuBar.addMenu()\
-
+        # Menu Bar
+        #self.menuBar = MenuBar.setMenuBar(self.menuBar)
+        #self.menuBar.addMenu()
 
 
         # End of menu bar
 
 
         # Layout add
-        #self.layout.setMenuBar(self.mainBar)
-
-
+        self.layout.setMenuBar(self.menuBar)
         self.setLayout(self.layout)
         self.setWindowTitle(self.tr("MAUS"))
-        self.layout.setMenuBar(self.mainMenu)
 
 
-    def createMenus(self):
-        pass
 
-        # fileMenu = menuBar().addMenu(tr("&File"))
 
-    # fileMenu.addAction(Act)
-    #
-    # fileMenu.addAction(openAct)
-    #
-    # fileMenu.addAction(saveAct)
-    # fileMenu.addAction(printAct)
-    #
-    # fileMenu.addSeparator()
-    #
-    # fileMenu.addAction(exitAct)
-    #
-    # editMenu = menuBar().addMenu(tr("&Edit"))
-    # editMenu.addAction(undoAct)
-    # editMenu.addAction(redoAct)
-    # editMenu.addSeparator()
-    # editMenu.addAction(cutAct)
-    # editMenu.addAction(copyAct)
-    # editMenu.addAction(pasteAct)
-    # editMenu.addSeparator()
-    #
-    # helpMenu = menuBar().addMenu(tr("&Help"))
-    # helpMenu.addAction(aboutAct)
-    # helpMenu.addAction(aboutQtAct)
-    #
+
 
