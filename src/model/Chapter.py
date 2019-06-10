0812a784982961cd22e2a84730215ac33063a7cc
Uros <blagojevic.uros99@yahoo.com>
2019-06-10 20:30:13 +0200

    Updated slike,textSlot

---
+++ src/model/Chapter.py
@@ -0,0 +1,22 @@
+from PySide2.QtCore import Signal
+from src.model.Node import Node
+from PySide2.QtGui import QIcon
+
+class Chapter(Node):
+
+    documentChangedSignal = Signal()
+
+    def __init__(self, name):
+        super(Chapter, self).__init__(name)
+        self.layout = None
+        self._name = None
+        self.elements = {}
+
+    def getIcon(self):
+        return QIcon('src/media/chapter.png')
+
+    def change(self):
+        self.documentChangedSignal.emit()
+
+    def set_name(self,name):
+        self._name = name
